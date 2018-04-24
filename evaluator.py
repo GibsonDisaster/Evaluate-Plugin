70d99b4e1d29df9de483d3bef4f4d7373fcd4c27
Henning Tonko <henninghtonko@gmail.com>
2018-04-24 12:14:44 -0500

    Added samples

--- evaluator.py
+++ evaluator.py
@@ -1,22 +1,39 @@
 import bpy
+import datetime as dt
 
 bl_info = {
     'name': 'Creating panels demonstation',
     'category': 'All'
 }
 
+'''
+    [] Timing
+    [X] Save original samples
+    [] Change samples to one
+    [] Render current frame
+    [] Label telling them to render from most complicated frame
+'''
+
 result = "N/A"
+sample_num = 0
+original_output = ""
    
 class EvalOp(bpy.types.Operator):
-    """Tooltip"""
+    """Eval"""
     bl_idname = "object.simple_operator"
     bl_label = "Simple Object Operator"
 
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
-
+        
     def execute(self, context):
+        sample_num = bpy.data.scenes["Scene"].cycles.samples
+        bpy.data.scenes["Scene"].cycles.samples = 1
+        original_output = bpy.data.scenes["Scene"].render.filepath
+        bpy.data.scenes["Scene"].render.filepath = "\\southw-sfps-01.business.mpls.k12.mn.us\\Students_M-Z\\hton1801\\Desktop\\THISONE.png"
+        bpy.ops.render.render(write_still=True)
+        bpy.data.scenes["Scene"].render.filepath = original_output
         return {'FINISHED'}
 
 class Properties(bpy.types.PropertyGroup):
@@ -25,7 +42,7 @@ class Properties(bpy.types.PropertyGroup):
     my_enum = bpy.props.EnumProperty(
         name="",
         description="GPU OR CPU",
-        items=[ ('OP1', "GPU", ""),
+        items= [('OP1', "GPU", ""),
                 ('OP2', "CPU", ""),
                ]
         )
@@ -44,7 +61,7 @@ class ActiveObject(bpy.types.Panel):
         obj = context.object
         
         row = layout.row()
-        row.label(text=result)
+        row.label(text=("Use: " + result))
         #layout.prop(mytool, "my_enum")
         row = layout.row()
         row.operator("object.simple_operator", "Evaluate")
@@ -52,7 +69,7 @@ class ActiveObject(bpy.types.Panel):
 def register():
     bpy.utils.register_class(ActiveObject)
     bpy.utils.register_class(Properties)
-    bpy.utils.register_class(SimpleOperator)
+    bpy.utils.register_class(EvalOp)
     bpy.types.Scene.my_tool = bpy.props.PointerProperty(type=Properties)
 
 def unregister():
